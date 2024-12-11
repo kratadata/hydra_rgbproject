923c3e8b599ea1cc7eb78d0a7fe04f8f5aa349ce
Paulina <48999145+kratadata@users.noreply.github.com>
2024-12-11 16:55:51 +0100

    update arduino

--- arduino/HYDRA_big/HYDRA_big/HYDRA_big.ino
+++ arduino/HYDRA_big/HYDRA_big/HYDRA_big.ino
@@ -1,18 +1,27 @@
 #define vaporizer 52
 
 const int ledPins[] = { 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+int numLeds = sizeof(ledPins) / sizeof(ledPins[0]);
 const int triggerPins[] = { 36, 32, 28, 24 };
 const int echoPins[] = { 34, 30, 26, 22 };
 
-int currentBrightness = 0;                    // Current brightness level
-int targetBrightness = 0;                     // Target brightness level based on distance
-unsigned long lastProximityCheck = 0;         // Last time proximity was checked
+int currentBrightness = 0;                     // Current brightness level
+int targetBrightness = 0;                      // Target brightness level based on distance
+unsigned long lastProximityCheck = 0;          // Last time proximity was checked
 const unsigned long proximityInterval = 100;  // Interval for checking proximity (in milliseconds)
-bool fadeEnabled = false;                     // Flag to indicate if fading should happen
+bool fadeEnabled = false;                      // Flag to indicate if fading should happen
+bool vaporizerEnabled = false;                 // Flag to indicate if vaporizers should be on
+unsigned long previousMillis = 0;
+int lightDistance = 150;                    // 100 cm
+int vaporDistance = 20;                     // 30 cm
 unsigned long distance1;
 unsigned long distance2;
 unsigned long distance3;
 unsigned long distance4;
+unsigned long lastUpdate = 0; 
+int updateInterval = 1;     
+int brightnessStep = 20;     
+
 
 void setup() {
   Serial.begin(19200);
@@ -47,34 +56,8 @@ void loop() {
     distance3 = proximity(triggerPins[2], echoPins[2], "dist3");
     distance4 = proximity(triggerPins[3], echoPins[3], "dist4");
 
-    // Map distance to target brightness
-    targetBrightness = map(constrain(distance1, 10, 100), 10, 100, 10, 255);
-
-    if (distance1 < 100 && distance1 > 0) {  // Ensure distance > 0 (valid reading)
-      fadeEnabled = true;
-    }
-
-    if (distance2 < 100 && distance2 > 0) {
-      digitalWrite(vaporizer, HIGH);
-    } else {
-      digitalWrite(vaporizer, LOW);
-    }
-  }
-
-
-  // Gradually adjust the brightness
-  if (fadeEnabled) {
-    if (currentBrightness < targetBrightness) {
-      currentBrightness += 5;
-    } else if (currentBrightness > targetBrightness) {
-      currentBrightness -= 5;
-    }
-
-    for (int i = 0; i < 11; i++) {
-      analogWrite(ledPins[i], currentBrightness);
-    }
-
-    delay(10);  // Short delay for smooth fading
+    controlVaporizer(distance2); //assign vaporizer to sensor 2
+    pulseLights(distance1);     //assign vaporizer to sensor 1
   }
 }
 
@@ -102,4 +85,51 @@ long proximity(int t, int e, const String& name) {
   Serial.println(" cm");
 
   return distance;
+}
+
+void controlVaporizer(long dist_sensor) {
+  // Vaporizer should be ON when distance is less than vaporDistance
+  if (dist_sensor < vaporDistance && dist_sensor > 0) {
+    digitalWrite(vaporizer, HIGH);
+    vaporizerEnabled = true;
+  } else {
+    digitalWrite(vaporizer, LOW);
+    vaporizerEnabled = false;
+  }
+}
+
+void pulseLights(long dist_sensor) {
+  // Pulsate mode when distance is less than lightDistance
+  if (dist_sensor < lightDistance && dist_sensor > 0) { 
+    fadeEnabled = true;
+    targetBrightness = map(constrain(dist_sensor, 0, lightDistance), 0, lightDistance, 10, 255);
+  } else { 
+    // Default to 10% brightness when not in range
+    fadeEnabled = false;
+    targetBrightness = 10; // 10% of 255 is approximately 26
+  }
+
+  // Update brightness based on timing
+  unsigned long currentMillis = millis();
+  if (currentMillis - lastUpdate >= updateInterval) {
+    lastUpdate = currentMillis;
+
+    // Smoothly adjust the brightness
+    if (currentBrightness < targetBrightness) {
+      currentBrightness += brightnessStep;
+      if (currentBrightness > targetBrightness) {
+        currentBrightness = targetBrightness;
+      }
+    } else if (currentBrightness > targetBrightness) {
+      currentBrightness -= brightnessStep;
+      if (currentBrightness < targetBrightness) {
+        currentBrightness = targetBrightness;
+      }
+    }
+
+    // Apply the brightness to all LEDs
+    for (int i = 0; i < numLeds; i++) {
+      analogWrite(ledPins[i], currentBrightness);
+    }
+  }
 }
